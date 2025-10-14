0eab9ce8779cda5ec0f502fd8ad15ffa8e2de448
钱启航 <17352819228@163.com>
2025-10-14 12:02:41 +0800

    10-14 实现矩阵类和基本运算函数

--- math/mathFunc.h
+++ math/mathFunc.h
@@ -2,6 +2,7 @@
 
 #include "../global/base.h"
 #include "vector.h"
+#include "matrix.h"
 
 namespace math {
 
@@ -143,5 +144,170 @@ namespace math {
     template<typename T>
     inline Vector4<T> normalize(const Vector4<T>& v) {
         return v / length(v);
+    }
+
+    //matrix
+    /*
+    * m0 m3 m6
+    * m1 m4 m7
+    * m2 m5 m8
+    */
+    template<typename T>
+    Matrix33<T> transpose(const Matrix33<T>& m) {
+        Matrix33<T> result;
+        auto dst = result.m;
+        auto src = m.m;
+        dst[0] = src[0]; dst[3] = src[1]; dst[6] = src[2];
+        dst[1] = src[3]; dst[4] = src[4]; dst[7] = src[5];
+        dst[2] = src[6]; dst[5] = src[7]; dst[8] = src[8];
+
+        return result;
+    }
+
+    /*
+     * m0 m4 m8  m12
+     * m1 m5 m9  m13
+     * m2 m6 m10 m14
+     * m3 m7 m11 m15
+     */
+    template<typename T>
+    Matrix44<T> transpose(const Matrix44<T>& m) {
+        Matrix44<T> result;
+        auto dst = result.m;
+        auto src = m.m;
+
+        dst[0] = src[0]; dst[4] = src[1]; dst[8] = src[2];  dst[12] = src[3];
+        dst[1] = src[4]; dst[5] = src[5]; dst[9] = src[6];  dst[13] = src[7];
+        dst[2] = src[8]; dst[6] = src[9]; dst[10] = src[10]; dst[14] = src[11];
+        dst[3] = src[12]; dst[7] = src[13]; dst[11] = src[14]; dst[15] = src[15];
+
+        return result;
+    }
+
+    template<typename T>
+    Matrix33<T> operator * (const Matrix33<T>& m1, const Matrix33<T>& m2) {
+        auto m1Col0 = m1.getColumn(0);
+        auto m1Col1 = m1.getColumn(1);
+        auto m1Col2 = m1.getColumn(2);
+
+        auto m2Col0 = m2.getColumn(0);
+        auto m2Col1 = m2.getColumn(1);
+        auto m2Col2 = m2.getColumn(2);
+
+        // 使用列视图进行计算
+        Vector3<T> rCol0, rCol1, rCol2;
+        rCol0 = m1Col0 * m2Col0[0] + m1Col1 * m2Col0[1] + m1Col2 * m2Col0[2];
+        rCol1 = m1Col0 * m2Col1[0] + m1Col1 * m2Col1[1] + m1Col2 * m2Col1[2];
+        rCol2 = m1Col0 * m2Col2[0] + m1Col1 * m2Col2[1] + m1Col2 * m2Col2[2];
+
+        Matrix33<T> result;
+        result.setColumn(rCol0, 0);
+        result.setColumn(rCol1, 1);
+        result.setColumn(rCol2, 2);
+
+        return result;
+    }
+
+    template<typename T>
+    Matrix44<T> operator * (const Matrix44<T>& m1, const Matrix44<T>& m2) {
+        auto m1Col0 = m1.getColumn(0);
+        auto m1Col1 = m1.getColumn(1);
+        auto m1Col2 = m1.getColumn(2);
+        auto m1Col3 = m1.getColumn(3);
+
+        auto m2Col0 = m2.getColumn(0);
+        auto m2Col1 = m2.getColumn(1);
+        auto m2Col2 = m2.getColumn(2);
+        auto m2Col3 = m2.getColumn(3);
+
+        // 使用列视图进行计算
+        Vector4<T> rCol0, rCol1, rCol2, rCol3;
+        rCol0 = m1Col0 * m2Col0[0] + m1Col1 * m2Col0[1] + m1Col2 * m2Col0[2] + m1Col3 * m2Col0[3];
+        rCol1 = m1Col0 * m2Col1[0] + m1Col1 * m2Col1[1] + m1Col2 * m2Col1[2] + m1Col3 * m2Col1[3];
+        rCol2 = m1Col0 * m2Col2[0] + m1Col1 * m2Col2[1] + m1Col2 * m2Col2[2] + m1Col3 * m2Col2[3];
+        rCol3 = m1Col0 * m2Col3[0] + m1Col1 * m2Col3[1] + m1Col2 * m2Col3[2] + m1Col3 * m2Col3[3];
+
+        Matrix44<T> result;
+        result.setColumn(rCol0, 0);
+        result.setColumn(rCol1, 1);
+        result.setColumn(rCol2, 2);
+        result.setColumn(rCol3, 3);
+
+        return result;
+    }
+
+    /*
+    * m0  m4  m8  m12
+    * m1  m5  m9  m13
+    * m2  m6  m10 m14
+    * m3  m7  m11 m15
+    */
+
+    template<typename T>
+    Matrix44<T> inverse(const Matrix44<T>& src) {
+        Matrix44<T> result(static_cast<T>(1));
+
+        // 计算每个必须的2*2矩阵行列式，下标是左上角到右下角
+        T D_22_33 = src.get(2, 2) * src.get(3, 3) - src.get(2, 3) * src.get(3, 2);
+
+        T D_12_33 = src.get(1, 2) * src.get(2, 3) - src.get(1, 3) * src.get(2, 2);
+        T D_12_23 = src.get(1, 2) * src.get(3, 3) - src.get(1, 3) * src.get(3, 2);
+
+        T D_21_32 = src.get(2, 1) * src.get(3, 2) - src.get(2, 2) * src.get(3, 1);
+        T D_21_33 = src.get(2, 1) * src.get(3, 3) - src.get(2, 3) * src.get(3, 1);
+
+        T D_11_22 = src.get(1, 1) * src.get(2, 2) - src.get(1, 2) * src.get(2, 1);
+        T D_11_23 = src.get(1, 1) * src.get(2, 3) - src.get(1, 3) * src.get(2, 1);
+        T D_11_32 = src.get(1, 1) * src.get(3, 2) - src.get(1, 2) * src.get(3, 1);
+        T D_11_33 = src.get(1, 1) * src.get(3, 3) - src.get(1, 3) * src.get(3, 1);
+
+        T D_02_13 = src.get(0, 2) * src.get(1, 3) - src.get(0, 3) * src.get(1, 2);
+        T D_02_23 = src.get(0, 2) * src.get(2, 3) - src.get(0, 3) * src.get(2, 2);
+        T D_02_33 = src.get(0, 2) * src.get(3, 3) - src.get(0, 3) * src.get(3, 2);
+
+        T D_01_12 = src.get(0, 1) * src.get(1, 2) - src.get(0, 2) * src.get(1, 1);
+        T D_01_13 = src.get(0, 1) * src.get(1, 3) - src.get(0, 3) * src.get(1, 1);
+        T D_01_22 = src.get(0, 1) * src.get(2, 2) - src.get(0, 2) * src.get(2, 1);
+        T D_01_23 = src.get(0, 1) * src.get(2, 3) - src.get(0, 3) * src.get(2, 1);
+        T D_01_32 = src.get(0, 1) * src.get(3, 2) - src.get(0, 2) * src.get(3, 1);
+        T D_01_33 = src.get(0, 1) * src.get(3, 3) - src.get(0, 3) * src.get(3, 1);
+
+        // 计算伴随矩阵的每列数据
+        Vector4<T> col0, col1, col2, col3;
+        col0.x = src.get(1, 1) * D_22_33 - src.get(2, 1) * D_12_33 + src.get(3, 1) * D_12_23;
+        col0.y = -(src.get(1, 0) * D_22_33 - src.get(2, 0) * D_12_33 + src.get(3, 0) * D_12_23);
+        col0.z = src.get(1, 0) * D_21_33 - src.get(2, 0) * D_11_33 + src.get(3, 0) * D_11_23;
+        col0.w = -(src.get(1, 0) * D_21_32 - src.get(2, 0) * D_11_32 + src.get(3, 0) * D_11_22);
+
+        col1.x = -(src.get(0, 1) * D_22_33 - src.get(2, 1) * D_02_33 + src.get(3, 1) * D_02_23);
+        col1.y = src.get(0, 0) * D_22_33 - src.get(2, 0) * D_02_33 + src.get(3, 0) * D_02_23;
+        col1.z = -(src.get(0, 0) * D_21_33 - src.get(2, 0) * D_01_33 + src.get(3, 0) * D_01_23);
+        col1.w = src.get(0, 0) * D_21_32 - src.get(2, 0) * D_01_32 + src.get(3, 0) * D_01_22;
+
+        col2.x = src.get(0, 1) * D_12_33 - src.get(1, 1) * D_02_33 + src.get(3, 1) * D_02_13;
+        col2.y = -(src.get(0, 0) * D_12_33 - src.get(1, 0) * D_02_33 + src.get(3, 0) * D_02_13);
+        col2.z = src.get(0, 0) * D_11_33 - src.get(1, 0) * D_01_33 + src.get(3, 0) * D_01_13;
+        col2.w = -(src.get(0, 0) * D_11_32 - src.get(1, 0) * D_01_32 + src.get(3, 0) * D_01_12);
+
+        col3.x = -(src.get(0, 1) * D_12_23 - src.get(1, 1) * D_02_23 + src.get(2, 1) * D_02_13);
+        col3.y = src.get(0, 0) * D_12_23 - src.get(1, 0) * D_02_23 + src.get(2, 0) * D_02_13;
+        col3.z = -(src.get(0, 0) * D_11_23 - src.get(1, 0) * D_01_23 + src.get(2, 0) * D_01_13);
+        col3.w = src.get(0, 0) * D_11_22 - src.get(1, 0) * D_01_22 + src.get(2, 0) * D_01_12;
+
+        result.setColumn(col0, 0);
+        result.setColumn(col1, 1);
+        result.setColumn(col2, 2);
+        result.setColumn(col3, 3);
+
+        // 计算行列式
+        Vector4<T> row0(result.get(0, 0), result.get(0, 1), result.get(0, 2), result.get(0, 3));
+        Vector4<T> colum0 = src.getColumn(0);
+        T determinant = dot(row0, colum0);
+
+        assert(determinant != 0);
+
+        T oneOverDeterminant = static_cast<T>(1) / determinant;
+
+        return result * oneOverDeterminant;
     }
 }
--- testMath.cpp
+++ testMath.cpp
@@ -1,9 +1,13 @@
 #include "global/base.h"
 #include "math/math.h"
+#include "math/matrix.h"
 
 using namespace math;
 
 int main() {
+    using namespace math;
+
+    // ===== 向量测试 =====
     vec2f v2_1(2.0f, 3.0f);
     vec2f v2_2 = v2_1;
 
@@ -25,4 +29,43 @@ int main() {
     vec4f v4(5.0f, 1.0f, 3.0f, 1.0f);
     //auto v4n = normalize(v4);
     //v4n.print();
+
+    // ===== 矩阵测试 =====
+    std::cout << "\n===== Matrix44 Test =====" << std::endl;
+    Mat4f mA;
+    mA.set(
+        1, 2, 3, 4,
+        5, 6, 7, 8,
+        9, 10, 11, 12,
+        0, 0, 0, 1);
+    std::cout << "Original Matrix:" << std::endl;
+    mA.printMatrix();
+
+    auto mATrans = math::transpose(mA);
+    std::cout << "Transpose:" << std::endl;
+    mATrans.printMatrix();
+
+    auto mB = mA * 2.0f; // 标量乘
+    std::cout << "Scalar *2:" << std::endl;
+    mB.printMatrix();
+
+    Mat4f identity = mA * math::inverse(mA); // 应当得到单位矩阵
+    std::cout << "A * inverse(A):" << std::endl;
+    identity.printMatrix();
+
+    // 乘以转置再求逆，结果应为 A
+    auto recover = math::inverse(mATrans) * mATrans;
+    std::cout << "inverse(Transpose) * Transpose:" << std::endl;
+    recover.printMatrix();
+
+    mA.set(
+        2, 0, 0, 0,
+        0, 4, 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 4);
+    std::cout << "inverse:" << std::endl;
+    mA = math::inverse(mA);
+    mA.printMatrix();
+
+    return 0;
 }
